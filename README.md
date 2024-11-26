ee92e1fe7477d4aa763c0090446d913c361a95bd
Legar Suryantara <legarsuryantara@gmail.com>
2024-11-26 10:29:41 +0700

    W11: Soal4

--- README.md
+++ README.md
@@ -12,3 +12,8 @@ Soal 3
  1. Substring adalah bagian dari string yang diambil dari string yang lebih besar. Dalam banyak bahasa pemrograman, Anda dapat menggunakan fungsi atau metode untuk mengekstrak substring dari string. </br>
  CatchError adalah operator yang digunakan untuk menangani kesalahan (error) yang mungkin terjadi dalam aliran data. Dengan catchError, dapat menangkap kesalahan dan memberikan penanganan alternatif, sehingga program tidak terhenti secara tiba-tiba
 ![screensoot](assets/images/003.gif)
+
+Soal 4
+1. Langkah 1 dalam kode yang diberikan mendefinisikan tiga fungsi asinkron: returnOneAsync(), returnTwoAsync(), dan returnThreeAsync(), yang masing-masing mengembalikan nilai integer 1, 2, dan 3 setelah menunggu selama 3 detik. Fungsi-fungsi ini menggunakan await Future.delayed(const Duration(seconds: 3)); untuk mensimulasikan operasi yang memerlukan waktu, seperti pengambilan data dari server. </br>
+langkah 2 mendefinisikan fungsi asinkron count(), yang bertujuan untuk menjumlahkan hasil dari ketiga fungsi tersebut. Di dalam fungsi ini, variabel total diinisialisasi dengan nilai 0, kemudian diisi dengan hasil dari masing-masing fungsi secara berurutan menggunakan await, sehingga setelah menjalankan ketiga fungsi, total akan berisi nilai 6. Terakhir, fungsi setState() dipanggil untuk memperbarui state dari widget, sehingga hasil penjumlahan yang baru dapat ditampilkan kepada pengguna.
+![screensoot](assets/images/004.gif)
--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:http/http.dart';
 import 'package:http/http.dart' as http;
 
-
 void main() {
   runApp(const MyApp());
 }
@@ -41,6 +40,31 @@ class _FuturePageState extends State<FuturePage> {
     return http.get(url);
   }
 
+  Future<int> returnOneAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 1;
+  }
+
+  Future<int> returnTwoAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 2;
+  }
+
+  Future<int> returnThreeAsync() async {
+    await Future.delayed(const Duration(seconds: 3));
+    return 3;
+  }
+
+  Future count() async {
+    int total = 0;
+    total = await returnOneAsync();
+    total += await returnTwoAsync();
+    total += await returnThreeAsync();
+    setState(() {
+      result = total.toString();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -54,16 +78,16 @@ class _FuturePageState extends State<FuturePage> {
             ElevatedButton(
               child: const Text('Go'),
               onPressed: () {
-                setState(() {});
-                getData()
-                .then((value) {
-                  result = value.body.toString().substring(0, 450);
-                  setState(() {});
-                })
-                .catchError((_){
-                  result = 'An error onccured';
-                  setState(() {});
-                });
+                // setState(() {});
+                // getData().then((value) {
+                //   result = value.body.toString().substring(0, 450);
+                //   setState(() {});
+                // }).catchError((_) {
+                //   result = 'An error onccured';
+                //   setState(() {});
+                // });
+
+                count();
               },
             ),
             const Spacer(),
